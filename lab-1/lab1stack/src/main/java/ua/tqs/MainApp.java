c5f0fdca0d7acd5d9f78ee6df34cfaa4ecee120e
Mariana Santos <marianasps@ua.pt>
2021-03-20 00:42:15 +0000

    small upd

--- lab-1/lab1stack/src/main/java/ua/tqs/MainApp.java
+++ lab-1/lab1stack/src/main/java/ua/tqs/MainApp.java
@@ -1,8 +1,7 @@
 package ua.tqs;
 
-public class App {
-    
+public class MainApp {
     public static void main(String[] args) {
+        System.out.println("Hello world");
     }
-    
 }
--- lab-1/lab1stack/src/test/java/ua/tqs/TqsStackTest.java
+++ lab-1/lab1stack/src/test/java/ua/tqs/TqsStackTest.java
@@ -7,10 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.NoSuchElementException;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 
 
 public class TqsStackTest {
